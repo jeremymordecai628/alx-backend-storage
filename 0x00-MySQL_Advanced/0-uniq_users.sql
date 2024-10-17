b8c058c437d31eea37cbb7fcf8fd935da3ac77b7
jeremymordecai628 <jeremymordecai08@gmail.com>
2024-10-17 21:38:51 +0300

    file change

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,14 +1,7 @@
--- Create the 'users' table if it does not already exist
-CREATE TABLE IF NOT EXISTS users (
-    -- 'id' column: An integer that cannot be NULL and auto-increments for each new record
-    id INT NOT NULL AUTO_INCREMENT,
-    
-    -- 'email' column: A string up to 255 characters, must be unique and cannot be NULL
+--- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    
-    -- 'name' column: A string up to 255 characters, can be NULL
-    name VARCHAR(255),
-    
-    -- Set 'id' as the primary key for the table
-    PRIMARY KEY (id)
-);
+    name VARCHAR(255)
+); 
